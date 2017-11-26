6c44c2ace547d49c2689a24a77b9b599121991b3
Elvin98 <33845833+Elvin98@users.noreply.github.com>
2017-11-25 21:52:53 -0600

    modificacion de codigo
    codigocon errores.. esperando mpodificacion

--- app/src/main/java/com/example/wolfware/agroapp/MainActivity.java
+++ app/src/main/java/com/example/wolfware/agroapp/MainActivity.java
@@ -5,18 +5,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.view.View.OnTouchListener;
 
 public class MainActivity extends Activity {
     //Necesitamos un Boton y un imageView
     private Button bt_hacerfoto;
     private ImageView img;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,57 @@ public class MainActivity extends Activity {
         //Relacionamos con el XML
         img = (ImageView)this.findViewById(R.id.imageView1);
         bt_hacerfoto = (Button) this.findViewById(R.id.button1);
+
+        img.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Bitmap bmpResult = ((BitmapDrawable)img.getDrawable()).getBitmap();
+                try{
+                    // Obtener las coordenadas sobre el imageView
+                    int x = (int)event.getX();
+                    int y = (int) event.getY();
+
+                    //Limitar x, y dentro del bitmap
+                    if(x < 0){
+                        x = 0;
+                    }else if(x > bmpResult.getWidth()-1){
+                        x = bmpResult.getWidth()-1;
+                    }
+                    if(y < 0){
+                        y = 0;
+                    }else if(y > bmpResult.getHeight()-1){
+                        y = bmpResult.getHeight()-1;
+                    }
+                    // Obtener el codigo decimal del pixel
+                    int pixel = bmpResult.getPixel(x,y);
+                    int rval = Color.red(pixel);
+                    int gval = Color.green(pixel);
+                    int bval = Color.blue(pixel);
+                    int iColor = Color.argb(255, rval, gval, bval);
+
+
+                   // txtRes.setBackgroundColor(iColor);
+
+                    // Calculo el CMYK Esto es de YAPA
+                    double rr = rval/ 255.0000;
+                    double gg = gval / 255.0000;
+                    double bb = bval / 255.0000;
+                    double black = 1.0000 - Math.max(rr,Math.max(gg,bb));
+                    double  cyan = (1-rr-black) / (1-black);
+                    double magenta = (1-gg-black) / (1-black);
+                    double yellow = (1-bb-black) / (1-black);
+
+
+                    // Muestro el resultado en HEXA
+                    System.out.println(String.format("RGB; #%06X   C:%.4f M:%.4f Y:%.4f K:%.4f", (0xFFFFFF & iColor),cyan,magenta,yellow,black));
+                }catch(Exception e){
+                    System.out.println("Error en pixel" + e.toString());
+                }
+                return true;
+            }
+        });
+
+
         //Añadimos el Listener Boton
         bt_hacerfoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +101,7 @@ public class MainActivity extends Activity {
                 //Lanzamos la aplicacion de la camara con retorno (forResult)
                 startActivityForResult(cameraIntent, 1);
             }});
+        //
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
