d8513821fb752e0b17ba5ec31ea3322f7adc91f8
Elvin98 <33845833+Elvin98@users.noreply.github.com>
2017-11-26 05:39:15 -0600

    commit

--- app/src/main/java/com/example/wolfware/agroapp/Main2Activity.java
+++ app/src/main/java/com/example/wolfware/agroapp/Main2Activity.java
@@ -24,7 +24,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.app_bar_principal);
         btnfoto = (Button) findViewById(R.id.btnfoto);
         imgimagen = (ImageView) findViewById(R.id.imgfoto);
         btnfoto.setOnClickListener(new View.OnClickListener() {
