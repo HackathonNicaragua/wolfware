2730a7d0327f641be197d45a2471e3dc980dbd24
Elvin98 <33845833+Elvin98@users.noreply.github.com>
2017-11-26 00:59:36 -0600

    comit

--- app/src/main/java/com/example/wolfware/agroapp/MainActivity.java
+++ app/src/main/java/com/example/wolfware/agroapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wolfware.agroapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Bitmap;
@@ -13,80 +14,39 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-    TextView touchedXY, invertedXY, imgSize, colorRGB;
-    ImageView imgSource1, imgSource2;
+    ImageView imagen;
+    TextView texto;
+    Bitmap map;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        touchedXY = (TextView)findViewById(R.id.xy);
-        invertedXY = (TextView)findViewById(R.id.invertedxy);
-        imgSize = (TextView)findViewById(R.id.size);
-        colorRGB = (TextView)findViewById(R.id.colorrgb);
-        imgSource1 = (ImageView)findViewById(R.id.source1);
-        imgSource2 = (ImageView)findViewById(R.id.source2);
+        texto= (TextView) findViewById(R.id.textView);
+        imagen= (ImageView) findViewById(R.id.imageView);
 
-        imgSource1.setOnTouchListener(imgSourceOnTouchListener);
-        imgSource2.setOnTouchListener(imgSourceOnTouchListener);
+        imagen.setDrawingCacheEnabled(true);
+        imagen.buildDrawingCache();
+
+        imagen.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()==MotionEvent.ACTION_DOWN||event.getAction()==MotionEvent.ACTION_MOVE)
+                    map=imagen.getDrawingCache();
+                    int pixel=map.getPixel((int) event.getX(), (int) event.getY());
+                    int r= Color.red(pixel);
+                    int g=Color.green(pixel);
+                    int b=Color.blue(pixel);
+
+                    texto.setBackgroundColor(Color.rgb(r,g,b));
+                    texto.setText("R: "+r+"\n"+"G: "+g+"\n"+"B: "+b);
+                return false;
+            }
+        });
+
 
     }
 
-    OnTouchListener imgSourceOnTouchListener
-            = new OnTouchListener(){
-
-        @Override
-        public boolean onTouch(View view, MotionEvent event) {
-
-            float eventX = event.getX();
-            float eventY = event.getY();
-            float[] eventXY = new float[] {eventX, eventY};
-
-            Matrix invertMatrix = new Matrix();
-            ((ImageView)view).getImageMatrix().invert(invertMatrix);
-
-            invertMatrix.mapPoints(eventXY);
-            int x = Integer.valueOf((int)eventXY[0]);
-            int y = Integer.valueOf((int)eventXY[1]);
-
-            touchedXY.setText(
-                    "touched position: "
-                            + String.valueOf(eventX) + " / "
-                            + String.valueOf(eventY));
-            invertedXY.setText(
-                    "touched position: "
-                            + String.valueOf(x) + " / "
-                            + String.valueOf(y));
-
-            Drawable imgDrawable = ((ImageView)view).getDrawable();
-            Bitmap bitmap = ((BitmapDrawable)imgDrawable).getBitmap();
-
-            imgSize.setText(
-                    "drawable size: "
-                            + String.valueOf(bitmap.getWidth()) + " / "
-                            + String.valueOf(bitmap.getHeight()));
-
-            //Limit x, y range within bitmap
-            if(x < 0){
-                x = 0;
-            }else if(x > bitmap.getWidth()-1){
-                x = bitmap.getWidth()-1;
-            }
-
-            if(y < 0){
-                y = 0;
-            }else if(y > bitmap.getHeight()-1){
-                y = bitmap.getHeight()-1;
-            }
-
-            int touchedRGB = bitmap.getPixel(x, y);
-
-            colorRGB.setText("touched color: " + "#" + Integer.toHexString(touchedRGB));
-            colorRGB.setTextColor(touchedRGB);
-
-            return true;
-        }};
 
 }
